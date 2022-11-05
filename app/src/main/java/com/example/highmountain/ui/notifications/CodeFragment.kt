89f19afdc8ca019547f977448b7c0e6aeba8385a
BlicBoy <terabyte1320@gmail.com>
2022-11-05 17:58:44 +0000

    Code

--- app/src/main/java/com/example/highmountain/ui/notifications/CodeFragment.kt
+++ app/src/main/java/com/example/highmountain/ui/notifications/CodeFragment.kt
@@ -28,7 +28,7 @@ class CodeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+
     }
 
     companion object {
