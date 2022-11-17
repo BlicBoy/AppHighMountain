e2284f91e420f0fc572f6fb23282bbf0b75532dc
BlicBoy <terabyte1320@gmail.com>
2022-11-17 13:34:09 +0000

    Class Percurso

--- app/src/main/java/com/example/highmountain/ui/LoadingDialog.kt
+++ app/src/main/java/com/example/highmountain/ui/LoadingDialog.kt
@@ -12,15 +12,17 @@ import androidx.annotation.RequiresApi
 import com.example.highmountain.R
 
 class LoadingDialog(val mActivity: Activity) {
+
     private lateinit var isdialog: AlertDialog
+
     fun startLoading(){
-        /**set View*/
         val infalter = mActivity.layoutInflater
         val dialogView = infalter.inflate(R.layout.loading_item,null)
-        /**set Dialog*/
+
         val bulider = AlertDialog.Builder(mActivity)
         bulider.setView(dialogView)
         bulider.setCancelable(false)
+
         isdialog = bulider.create()
         isdialog.show()
     }
