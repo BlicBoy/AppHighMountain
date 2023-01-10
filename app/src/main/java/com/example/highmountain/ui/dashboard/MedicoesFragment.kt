f269b0f5a25cd233f6f5f82251168bddf49e454c
Gonçalo Oliveira <67290150+BlicBoy@users.noreply.github.com>
2023-01-10 16:05:57 +0000

    latitude, longitude e altura

--- app/src/main/java/com/example/highmountain/ui/dashboard/MedicoesFragment.kt
+++ app/src/main/java/com/example/highmountain/ui/dashboard/MedicoesFragment.kt
@@ -1,31 +1,26 @@
 package com.example.highmountain.ui.dashboard
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
+import  android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import androidx.fragment.app.Fragment
 import com.example.highmountain.databinding.FragmentMedicoesBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MedicoesFragment : Fragment() {
 
     private var _binding : FragmentMedicoesBinding?= null
     private val binding get() = _binding!!
-
-
-
+    private lateinit var datagps : TextView
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
 
     override fun onCreateView(
@@ -38,45 +33,39 @@ class MedicoesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        datagps = binding.textViewDatagps
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocation()
+
 
     }
+    
+    private fun getLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 100)
+            return
+        }
 
-    private fun checkGPSEnabled(): Boolean {
-        if (!isLocationEnabled())
-        return isLocationEnabled()
-    }
 
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-       when(requestCode){
-           REQUEST_LOCATION_CODE -> {
-               // If request is cancelled, the result arrays are empty.
-               if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   // permission was granted, yay! Do the location-related task you need to do.
-                   if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                       Toast.makeText(requireContext(), "permission granted", Toast.LENGTH_LONG).show()
-                   }
-               } else {
-                   // permission denied, boo! Disable the functionality that depends on this permission.
-                   Toast.makeText(requireContext(), "permission denied", Toast.LENGTH_LONG).show()
-               }
-               return
-           }
-       }
-
+        val location = fusedLocationProviderClient.lastLocation
+        location.addOnSuccessListener {
+            if(it != null){
+              datagps.setText("Longitude:" +it.longitude.toString()+"Latitude:  "+it.latitude.toString()+ "Altura: " + it.altitude.toString())
+            }
+        }
     }
 
 
 
 
     companion object {
-
-        val LOCATION_PERMISSION_CODE = 1000
-        val REQUEST_LOCATION_CODE = 101
 
     }
 }
